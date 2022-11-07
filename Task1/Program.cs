589a2b6ba93c5aa1afd2b8225fb42fc3f8cabf5c
Vladimir <grandtwix33@mail.ru>
2022-11-07 22:40:32 +0500

    add number in Task1

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,2 +1,13 @@
-﻿// 
-Console.WriteLine("Hello, World!");
+﻿// Дано число обозначающее день недели. Выяснить является ли номер дня недели выходным
+Console.Clear();
+Console.Write("Введите число от 1 до 7: ");
+int number = int.Parse(Console.ReadLine());
+if (number == 6 || number == 7)
+{
+    Console.WriteLine($"Номер {number} является выходным днём");
+}
+else
+{
+    Console.WriteLine($"Номер {number} не является выходным днём");
+}
+
